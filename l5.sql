07e703e9e63b63d5078bafa0e5ddde59e9bc7ea0
Clusiv <sys_adm95@inbox.ru>
2022-08-31 16:52:54 +0300

    l5T3 included select
    added solution for 3 task

--- l5.sql
+++ l5.sql
@@ -5,9 +5,16 @@ SELECT * FROM north_american_cities
 where country = 'United States'
 order by latitude desc;
 
+#3
 SELECT * FROM north_american_cities
 where longitude < -87.629798
 order by longitude asc;
+
+#3 
+SELECT * FROM north_american_cities
+where longitude < (select longitude from north_american_cities where city = "Chicago")
+order by longitude asc
+
 
 SELECT * FROM north_american_cities
 where country = 'Mexico'
